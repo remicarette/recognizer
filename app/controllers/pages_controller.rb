43bac375f89181871e62d3ccf2b646bf5855ff4c
remi carette <carette.remi@gmail.com>
2020-08-07 17:01:48 +0200

    ok

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,6 @@
 class PagesController < ApplicationController
   def home
-    puts "========= home =========="
-    image_url = 'https://res.cloudinary.com/dn3zpeebk/image/upload/v1596795234/ticket_ynbg8h.jpg'
-    image = RTesseract.new('https://www.drillster.com/info/img/screenshot-ticket-received.en.png')
-    puts "=========== image ========="
-    p image
+    image = RTesseract.new('https://res.cloudinary.com/dn3zpeebk/image/upload/v1596795234/ticket_ynbg8h.jpg')
     @result = image.to_s
   end
 end
